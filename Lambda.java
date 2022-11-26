e6b6a9407dc869e96dd99dee03243d8a163c1a7c
Nandha kumar S <116502195+Nandha-S@users.noreply.github.com>
2022-11-26 21:15:22 +0530

    Java Program

---
+++ Lambda.java
@@ -0,0 +1,12 @@
+package javaPrograms;
+interface Test{
+	int m(int a,int b);
+}
+
+public class Lambda {
+ public static void main(String[] args) {
+	Test t=(a,b)->{
+		return a+b;
+	};System.out.println(t.m(50,45));
+}
+}
